e05761e1c9927a52c71ae5abe0608c2fa3ef8b0e
Nathan Marrero Petruci <nathan_marrero@hotmail.com>
2022-09-29 22:03:10 -0300

    Retirada do console.log().

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -40,7 +40,6 @@ class ImportCategoryUseCase {
     }
 
     async execute(file: Express.Multer.File): Promise<void> {
-        console.log(file);
         const categories = await this.loadCategories(file);
         categories.map(async (category) => {
             const { name, description } = category;
